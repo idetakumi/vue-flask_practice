e5791f377f06c4c01cfcd4e4ad3b57563dee1cb1
idetakumi <boc.10194@gmail.com>
2019-04-07 22:41:46 +0900

    Updated for heroku

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,8 @@
 import os
+from backend import app
 class BaseConfig(object):
     DEBUG = True
     ## 以下を追記
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///backend.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or "postgresql://postgres:postgres@localhost:5432/flask_db"
     # cookieを暗号化する秘密鍵
     SECRET_KEY = os.urandom(24)
